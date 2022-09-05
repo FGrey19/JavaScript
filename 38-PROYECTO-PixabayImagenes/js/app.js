0ad108921b15f33a81631c4c4751a4a5f66e75b7
Fernando Carrasco <ferdmcg19@gmail.com>
2022-09-04 22:59:25 -0500

    API de imágenes con Pixabay

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -1,8 +1,14 @@
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
+const paginacionDiv = document.querySelector('#paginacion');
+
+const registrosPorPagina = 40;
+let totalPaginas;
+let iterador;
+let paginaActual = 1;
 
 window.onload = () => {
-    formulario.addEventListener('submit', validarFormulario)
+    formulario.addEventListener('submit', validarFormulario);
 }
 
 function validarFormulario(e) {
@@ -14,6 +20,8 @@ function validarFormulario(e) {
         mostrarAlerta('Agrega un término de búsqueda');
         return;
     }
+
+    buscarImagenes();
 }
 
 function mostrarAlerta(mensaje) {
@@ -35,6 +43,96 @@ function mostrarAlerta(mensaje) {
             alerta.remove();
         }, 3000);
     }   
+}
+
+function buscarImagenes() {
+
+    const termino = document.querySelector('#termino').value;
+
+    const key = '29715672-dececc1e06947a8c15e9ced79';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            totalPaginas = calcularPaginas(resultado.totalHits);
+            mostrarImagenes(resultado.hits);
+        })
+}
+
+//Generador que va a registrar la cantidad de elementos de acuerdo a las páginas
+
+function *crearPaginador(total) {
+    for (let i = 1; i <= total; i++) {
+        yield i 
+    }
+}
+
+
+function calcularPaginas(total) {
+    return parseInt(Math.ceil( total / registrosPorPagina ));
+}
+
+function mostrarImagenes(imagenes) {
+
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+
+    }
+
+    //Iterar sobre el array de imagenes y construir el html
+    imagenes.forEach( imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class='w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4'>
+                <div class='bg-white'>
+                    <a href=${largeImageURL} target='_blank' rel='noopener noreferrer'>
+                    <img class='w-full' src=${previewURL}>
+                    </a>
+
+                    <div class='p-4'>
+                        <p class='font-bold'> ${likes} <span class='font-light'> Me gusta </span></p>
+                        <p class='font-bold'> ${views} <span class='font-light'> Vistas </span></p>
+
+
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    // Limpiar paginador previo
+
+    while (paginacionDiv.firstChild) {
+        paginacionDiv.removeChild(paginacionDiv.firstChild);
+    }
+
+    imprimirPaginador();
+}
+
+function imprimirPaginador() {
+    iterador = crearPaginador(totalPaginas);
+
+    while (true) {
+        const {value, done} = iterador.next();
+        if(done) return;
+
+        //Caso contrario, genera un botón por cada elemento en el generador
+        const boton = document.createElement('a');
+        boton.href = '#';
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente','bg-yellow-400','px-4','py-1','mr-2','font-bold','mb-5','rounded');
+
+        boton.onclick = () => {
+            paginaActual = value;
+
+            buscarImagenes()
+        }
+
+        paginacionDiv.appendChild(boton);
+    }
 }
 
 
